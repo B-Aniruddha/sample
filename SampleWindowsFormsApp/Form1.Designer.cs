51b67cf3a73de70f5b7c352da136ddeaf893114f
B-Aniruddha <70235936+B-Aniruddha@users.noreply.github.com>
2021-02-16 11:12:19 -0500

    Initial commit

---
+++ SampleWindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace SampleWindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DefaultButton = new System.Windows.Forms.Button();
+            this.TextBoxOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // DefaultButton
+            // 
+            this.DefaultButton.Location = new System.Drawing.Point(69, 75);
+            this.DefaultButton.Name = "DefaultButton";
+            this.DefaultButton.Size = new System.Drawing.Size(75, 23);
+            this.DefaultButton.TabIndex = 0;
+            this.DefaultButton.Text = "Default";
+            this.DefaultButton.UseVisualStyleBackColor = true;
+            this.DefaultButton.Click += new System.EventHandler(this.DefaultButton_Click);
+            // 
+            // TextBoxOutput
+            // 
+            this.TextBoxOutput.Location = new System.Drawing.Point(69, 116);
+            this.TextBoxOutput.Multiline = true;
+            this.TextBoxOutput.Name = "TextBoxOutput";
+            this.TextBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.TextBoxOutput.Size = new System.Drawing.Size(653, 215);
+            this.TextBoxOutput.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TextBoxOutput);
+            this.Controls.Add(this.DefaultButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DefaultButton;
+        private System.Windows.Forms.TextBox TextBoxOutput;
+    }
+}
+
---
+++ SampleWindowsFormsApp/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleWindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static void WriteMe(out int x) {
+            x = 1;
+        }
+
+        private void DefaultButton_Click(object sender, EventArgs e)
+        {
+            byte op1 = 1;
+            sbyte op2 = -1;
+            var result = op1 & op2;
+ 
+            TextBoxOutput.Text += result.GetType().Name;
+
+        }
+
+
+    }
+
+    enum Colors1 { 
+    B = 1,
+    R,
+    Y=4,
+    P = B | R,
+    G = Y | B,
+    O = R | Y
+    }
+ 
+}
